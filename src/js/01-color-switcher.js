8757ba920d7426dc15a31b3daaae3b21cbbe2d9f
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-21 03:21:30 -0500

    Asignacion de color aleatorio

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,28 @@
 
 
-function getRandomHexColor() {
+const getRandomHexColor = () =>{
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-  
+const changeBackgroungColor = () => {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+let intervalId;
 const start = document.querySelector('button[data-start]');
 const stop = document.querySelector('button[data-stop]');
 stop.disabled = true;
 
+//agregamos el evento click sobre el botton start
 start.addEventListener('click', function() {
-    console.log('click en Start');
-    document.body.style.backgroundColor = getRandomHexColor();
     start.disabled = true;
     stop.disabled = false;
-
+    intervalId = setInterval(changeBackgroungColor, 1000);
 });
 
+//agregamos el evento click sobre el botton stop
 stop.addEventListener('click', function() {
-    console.log('click en Stop');
     start.disabled = false ;
     stop.disabled = true;
-    document.body.style.backgroundColor = 'white';
+    clearInterval(intervalId);
 });
