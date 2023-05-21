74fb59f9f78b1828ae9c85ac5f45e27740b3ef0a
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-21 02:54:23 -0500

    Punto uno color fijo
    sin intervalo de tiempo

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+  
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+stop.disabled = true;
+
+start.addEventListener('click', function() {
+    console.log('click en Start');
+    document.body.style.backgroundColor = getRandomHexColor();
+    start.disabled = true;
+    stop.disabled = false;
+
+});
+
+stop.addEventListener('click', function() {
+    console.log('click en Stop');
+    start.disabled = false ;
+    stop.disabled = true;
+    document.body.style.backgroundColor = 'white';
+});
