4219639e6ae09521e881927c557ee721ecb6502e
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-21 20:21:37 -0500

    inicio tarea 2
    instalacion de librerias

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,35 @@
 
+let intervalId; // Variable parael intervalo de tiempo
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+stop.disabled = true;// Ocultamos el boton stop
 
+//Funcion para obtener un color aleatorio
 const getRandomHexColor = () =>{
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+// Funcion para obtener un fondo aleatorio
 const changeBackgroungColor = () => {
     document.body.style.backgroundColor = getRandomHexColor();
 }
 
-let intervalId;
-const start = document.querySelector('button[data-start]');
-const stop = document.querySelector('button[data-stop]');
-stop.disabled = true;
-
-//agregamos el evento click sobre el botton start
-start.addEventListener('click', function() {
+//Funcion para iniciar la ejecucion del intervalo
+const onInterval = () =>{
     start.disabled = true;
     stop.disabled = false;
+// Metodo interval para repeteir el codigo cada segundo
     intervalId = setInterval(changeBackgroungColor, 1000);
-});
+};
 
-//agregamos el evento click sobre el botton stop
-stop.addEventListener('click', function() {
+//Funcion para detener la ejecucion  del intervalo
+const clearInterval = () => {
     start.disabled = false ;
     stop.disabled = true;
+// Metodo para detener el intervalo de tiempo de start
     clearInterval(intervalId);
-});
+}
+
+// Leemos los eventos en los botones
+startButton.addEventListener('click', onInterval);
+stopButton.addEventListener('click', clearInterval);
