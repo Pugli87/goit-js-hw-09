f2ec0227403697581b2e658ebcee6edf7aca46af
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-22 01:54:32 +0000

    Deploying to gh-pages from @ Pugli87/goit-js-hw-09@a7eb7d23b39bc70ebf7c8072b4167666e94fe221 🚀

---
+++ 01-color-switcher.0a8149e8.js
@@ -0,0 +1,2 @@
+let t;const e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");stop.disabled=!0;const a=()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},o=()=>{start.disabled=!1,stop.disabled=!0,o(t)};e.addEventListener("click",(()=>{e.disabled=!0,d.disabled=!1,t=setInterval(a,1e3)})),d.addEventListener("click",o);
+//# sourceMappingURL=01-color-switcher.0a8149e8.js.map
