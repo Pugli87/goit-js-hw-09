fd24d49da9a8df3664dcdd96c0986450aea97aa4
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-22 04:01:32 +0000

    Deploying to gh-pages from @ Pugli87/goit-js-hw-09@c294534a8a43b0f940f488e57b5638d958aa7dbc 🚀

--- 01-color-switcher.9afedad7.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");stop.disabled=!0;var a=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))},n=function(){e.disabled=!1,d.disabled=!0,n(t)};e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,t=setInterval(a,1e3)})),d.addEventListener("click",n)}();
-//# sourceMappingURL=01-color-switcher.9afedad7.js.map
