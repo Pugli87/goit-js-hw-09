8865cc0aa6c670e5723bae82a18c41be3898923b
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-28 22:32:53 -0500

    Punto 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,11 +3,22 @@ import Notiflix from "notiflix";
 // Obtener referencia al formulario
 const form = document.querySelector('.form');
 
-// Escuchar el evento de envío del formulario
-form.addEventListener('submit', handleSubmit);
+// Función para crear una promesa
+const  createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-// Función para manejar el envío del formulario
-function handleSubmit(event) {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const handleSubmit = (event) => {
   event.preventDefault();
 
   // Obtener los valores del formulario
@@ -15,31 +26,16 @@ function handleSubmit(event) {
   const step = parseInt(form.elements.step.value);
   const amount = parseInt(form.elements.amount.value);
 
-  // Función para crear una promesa
-  function createPromise(position, delay) {
-    return new Promise((resolve, reject) => {
-      const shouldResolve = Math.random() > 0.3;
-  
-      setTimeout(() => {
-        if (shouldResolve) {
-          resolve({ position, delay });
-        } else {
-          reject({ position, delay });
-        }
-      }, delay);
-    });
-  }
-  
-  // Generar las promesas
+  // Generardor de las promesas
   for (let i = 0; i < amount; i++) {
     createPromise(i + 1, delay + step * i)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        // Aquí puedes agregar código para mostrar notificaciones con notiflix
       })
       .catch(({ position, delay }) => {
         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        // Aquí puedes agregar código para mostrar notificaciones con notiflix
       });
   }
 }
+
+form.addEventListener('submit', handleSubmit);
